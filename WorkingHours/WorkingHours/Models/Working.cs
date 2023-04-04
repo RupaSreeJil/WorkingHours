61053e4062b7b045c6c38ef685e28c8478831a9f
RupaSreeJil <129830465+RupaSreeJil@users.noreply.github.com>
2023-04-04 12:12:36 +0530

    Intialcheckin

---
+++ WorkingHours/WorkingHours/Models/Working.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkingHours.Models
+{
+    public class Working
+    {
+        private DbContextOptions<Working> options;
+
+        public Working(DbContextOptions<Working> options)
+        {
+            this.options = options;
+        }
+
+        public int HoursWorked { get; set; }
+        public int RegularHours { get; set; }
+        public int OvertimeWorkingHrs { get; set; }
+    }
+}
---
+++ WorkingHours/WorkingHours/Models/WorkingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Data.Entity;
+
+namespace WorkingHours.Models
+{
+    public class WorkingDbContext : DbContext
+    {
+        public WorkingDbContext(DbContextOptions<WorkingDbContext>options) :base(options)
+            { }
+    }
+    public DbSet<Working> Working{ get; set; }
+}
